e79d88403180248eb1b619a19e6cbbbebc1287df
Kiliman <kiliman@gmail.com>
2019-06-19 17:06:14 -0400

    Display styled items on home page

--- sick-fits/frontend/components/Items.js
+++ sick-fits/frontend/components/Items.js
@@ -3,6 +3,8 @@ import { Query } from 'react-apollo'
 import gql from 'graphql-tag'
 import styled from 'styled-components'
 
+import Item from './Item'
+
 const ALL_ITEMS_QUERY = gql`
   query ALL_ITEMS_QUERY {
     items {
@@ -22,16 +24,16 @@ const Center = styled.div`
 
 const ItemsList = styled.div`
   display: grid;
-  grid-auto-columns: 1fr 1ft;
+  grid-template-columns: 1fr 1fr;
   grid-gap: 60px;
   max-width: ${props => props.theme.maxWidth};
   margin: 0 auto;
 `
+
 export default class Items extends Component {
   render() {
     return (
-      <div>
-        <p>Items</p>
+      <Center>
         <Query query={ALL_ITEMS_QUERY}>
           {({ data, error, loading }) => {
             if (loading) return <p>Loading...</p>
@@ -39,13 +41,13 @@ export default class Items extends Component {
             return (
               <ItemsList>
                 {data.items.map(item => (
-                  <p key={item.id}>{item.title}</p>
+                  <Item key={item.id} item={item} />
                 ))}
               </ItemsList>
             )
           }}
         </Query>
-      </div>
+      </Center>
     )
   }
 }
