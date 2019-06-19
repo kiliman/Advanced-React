5448ff8de15c204deb47c2be8991de75dc5ef832
Kiliman <kiliman@gmail.com>
2019-06-19 14:54:56 -0400

    Display items using graphql query

---
+++ sick-fits/frontend/components/Items.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+import { Query } from 'react-apollo'
+import gql from 'graphql-tag'
+import styled from 'styled-components'
+
+const ALL_ITEMS_QUERY = gql`
+  query ALL_ITEMS_QUERY {
+    items {
+      id
+      title
+      price
+      description
+      image
+      largeImage
+    }
+  }
+`
+
+const Center = styled.div`
+  text-align: center;
+`
+
+const ItemsList = styled.div`
+  display: grid;
+  grid-auto-columns: 1fr 1ft;
+  grid-gap: 60px;
+  max-width: ${props => props.theme.maxWidth};
+  margin: 0 auto;
+`
+export default class Items extends Component {
+  render() {
+    return (
+      <div>
+        <p>Items</p>
+        <Query query={ALL_ITEMS_QUERY}>
+          {({ data, error, loading }) => {
+            if (loading) return <p>Loading...</p>
+            if (error) return <p>Error: {error.message}</p>
+            return (
+              <ItemsList>
+                {data.items.map(item => (
+                  <p key={item.id}>{item.title}</p>
+                ))}
+              </ItemsList>
+            )
+          }}
+        </Query>
+      </div>
+    )
+  }
+}
