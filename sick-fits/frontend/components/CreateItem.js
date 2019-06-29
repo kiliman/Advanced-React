16974373d471f211090eea6d236f8b4a6a46dbb2
Kiliman <kiliman@gmail.com>
2019-06-29 16:06:04 -0400

    Add ability to upload images for new items (lesson 18)

--- sick-fits/frontend/components/CreateItem.js
+++ sick-fits/frontend/components/CreateItem.js
@@ -6,8 +6,6 @@ import gql from 'graphql-tag'
 import Form from './styles/Form'
 import ErrorMessage from './ErrorMessage'
 
-import formatMoney from '../lib/formatMoney'
-
 const CREATE_ITEM_MUTATION = gql`
   mutation CREATE_ITEM_MUTATION(
     $title: String!
@@ -30,16 +28,38 @@ const CREATE_ITEM_MUTATION = gql`
 
 class CreateItem extends Component {
   state = {
-    title: 'Cool Shoes',
-    description: 'I love these shoes',
-    image: 'shoes.jpg',
-    largeImage: 'shoes_large.jpg',
-    price: 1000,
+    title: '',
+    description: '',
+    image: '',
+    largeImage: '',
+    price: 0,
   }
   handleChange = e => {
     const { name, type, value } = e.target
     const val = type === 'number' ? parseFloat(value) : value
     this.setState({ [name]: val })
+  }
+
+  uploadFile = async e => {
+    console.log('uploading file...')
+    const files = e.target.files
+    const data = new FormData()
+    data.append('file', files[0])
+    data.append('upload_preset', 'sickfits')
+
+    const res = await fetch(
+      'https://api.cloudinary.com/v1_1/kiliman/image/upload',
+      {
+        method: 'POST',
+        body: data,
+      },
+    )
+    const file = await res.json()
+    console.log(file)
+    this.setState({
+      image: file.secure_url,
+      largeImage: file.eager[0].secure_url,
+    })
   }
 
   render() {
@@ -58,6 +78,24 @@ class CreateItem extends Component {
           >
             <ErrorMessage error={error} />
             <fieldset disabled={loading} aria-busy={loading}>
+              <label htmlFor="file">
+                Image
+                <input
+                  type="file"
+                  id="file"
+                  name="file"
+                  placeholder="Upload an image"
+                  required
+                  onChange={this.uploadFile}
+                />
+                {this.state.image && (
+                  <img
+                    src={this.state.image}
+                    width={200}
+                    alt="Upload preview"
+                  />
+                )}
+              </label>
               <label htmlFor="title">
                 Title
                 <input
