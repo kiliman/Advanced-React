6b6bfe9aeec12f3ccd7e881acfba479f5651cb9b
Kiliman <kiliman@gmail.com>
2019-06-29 19:25:48 -0400

    Update query to use pagination (lesson 23)

--- sick-fits/frontend/components/Items.js
+++ sick-fits/frontend/components/Items.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import { Query } from 'react-apollo'
 import gql from 'graphql-tag'
 import styled from 'styled-components'
 
+import { perPage } from '../config'
 import Pagination from './Pagination'
 import Item from './Item'
 
 const ALL_ITEMS_QUERY = gql`
-  query ALL_ITEMS_QUERY {
-    items {
+  query ALL_ITEMS_QUERY($skip: Int = 0) {
+    items(skip: $skip, orderBy: createdAt_DESC) {
       id
       title
       price
@@ -32,12 +34,19 @@ const ItemsList = styled.div`
 `
 
 class Items extends Component {
+  static propTypes = {
+    page: PropTypes.number,
+  }
+
   render() {
     const { page } = this.props
     return (
       <Center>
         <Pagination page={page} />
-        <Query query={ALL_ITEMS_QUERY}>
+        <Query
+          query={ALL_ITEMS_QUERY}
+          variables={{ skip: (page - 1) * perPage }}
+        >
           {({ data, error, loading }) => {
             if (loading) return <p>Loading...</p>
             if (error) return <p>Error: {error.message}</p>
