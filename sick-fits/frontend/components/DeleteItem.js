63af8f2559d367313ec6e4ac9479af460dc7a58e
Kiliman <kiliman@gmail.com>
2019-06-29 17:43:39 -0400

    Add DeleteItem component to delete item and update cache (lesson 20)

---
+++ sick-fits/frontend/components/DeleteItem.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { Mutation } from 'react-apollo'
+import gql from 'graphql-tag'
+
+import { ALL_ITEMS_QUERY } from './Items'
+
+const DELETE_ITEM_MUTATION = gql`
+  mutation DELETE_ITEM_MUTATION($id: ID!) {
+    deleteItem(id: $id) {
+      id
+    }
+  }
+`
+class DeleteItem extends Component {
+  static propTypes = {
+    id: PropTypes.string.isRequired,
+    children: PropTypes.node,
+  }
+
+  handleClick = (e, deleteItem) => {
+    if (confirm('Are you sure you want to delete this item?')) {
+      deleteItem()
+    }
+  }
+
+  update = (cache, payload) => {
+    // manually update the cache on the client so it matches the server
+    // 1. Read the cache for the items we want
+    const data = cache.readQuery({ query: ALL_ITEMS_QUERY })
+    // 2. Filter the deleted item out of the cache
+    data.items = data.items.filter(
+      item => item.id !== payload.data.deleteItem.id,
+    )
+    // 3. Put the items back
+    cache.writeQuery({ query: ALL_ITEMS_QUERY, data })
+  }
+  render() {
+    return (
+      <Mutation
+        mutation={DELETE_ITEM_MUTATION}
+        variables={{ id: this.props.id }}
+        update={this.update}
+      >
+        {(deleteItem, { error }) => {
+          return (
+            <button onClick={e => this.handleClick(e, deleteItem)}>
+              {this.props.children}
+            </button>
+          )
+        }}
+      </Mutation>
+    )
+  }
+}
+
+export default DeleteItem
--- sick-fits/frontend/components/Item.js
+++ sick-fits/frontend/components/Item.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Link from 'next/link'
 
+import DeleteItem from './DeleteItem'
+
 import Title from './styles/Title'
 import ItemStyles from './styles/ItemStyles'
 import PriceTag from './styles/PriceTag'
@@ -31,7 +33,7 @@ export default class Item extends Component {
             <a>Edit ✏️</a>
           </Link>
           <button>Add to Cart</button>
-          <button>Delete</button>
+          <DeleteItem id={item.id}>Delete This Item</DeleteItem>
         </div>
       </ItemStyles>
     )
