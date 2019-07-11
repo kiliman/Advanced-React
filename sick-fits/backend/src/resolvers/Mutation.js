9c5129076639f5484582a9310a5745914f662444
Kiliman <kiliman@gmail.com>
2019-07-11 18:09:19 -0400

    Add signup mutation (lesson 24)

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
 const Mutations = {
   async createItem(parent, args, ctx, info) {
     const item = await ctx.db.mutation.createItem(
@@ -32,6 +35,29 @@ const Mutations = {
     // TODO
     // 3. delete it
     return ctx.db.mutation.deleteItem({ where }, info)
+  },
+  async signup(parent, args, ctx, info) {
+    args.email = args.email.toLowerCase()
+    // hash their password
+    const hash = await bcrypt.hash(args.password, 10)
+    const user = await ctx.db.mutation.createUser(
+      {
+        data: {
+          ...args,
+          password: hash,
+          permissions: { set: ['USER'] },
+        },
+      },
+      info,
+    )
+    // create JWT token
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET)
+    // set the cookie
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year
+    })
+    return user
   },
 }
 
